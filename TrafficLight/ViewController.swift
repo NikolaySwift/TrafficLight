40409835b75e93112ddce225f8c65fcff07860d3
NikolayD <volodar86@yandex.ru>
2024-06-30 16:08:12 +0300

    for each view add IBOutlet in ViewController. set their colours, alpha and corner radius.

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        redView.backgroundColor = UIColor.systemRed
+        redView.layer.cornerRadius = redView.frame.size.width / 2
+        redView.alpha = 0.3
+        
+        yellowView.backgroundColor = UIColor.systemYellow
+        yellowView.layer.cornerRadius = yellowView.frame.size.width / 2
+        yellowView.alpha = 0.3
+        
+        greenView.backgroundColor = UIColor.systemGreen
+        greenView.layer.cornerRadius = yellowView.frame.size.width / 2
+        greenView.alpha = 0.3
     }
-
 
 }
 
