5c721a0cb045be2157e9482701eadf6b719d90ff
NikolayD <volodar86@yandex.ru>
2024-06-30 18:32:39 +0300

    make enum with traffic light condition. make private method to switch light. realise switching of traffic light.

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 final class ViewController: UIViewController {
+    
+    enum TrafficLight {
+        case switchOff
+        case red
+        case yellow
+        case green
+    }
 
     @IBOutlet var redView: UIView!
     @IBOutlet var yellowView: UIView!
     @IBOutlet var greenView: UIView!
     
     @IBOutlet var switchLightButton: UIButton!
+    
+    private var currentLight: TrafficLight = .switchOff
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,13 +36,32 @@ final class ViewController: UIViewController {
     }
     
     @IBAction func switchLightButtonDidTapped(_ sender: UIButton) {
-        
+        switch currentLight {
+        case .red:
+            currentLight = .yellow
+            switchLight(from: redView, to: yellowView)
+        case .yellow:
+            currentLight = .green
+            switchLight(from: yellowView, to: greenView)
+        case .green:
+            currentLight = .red
+            switchLight(from: greenView, to: redView)
+        case .switchOff:
+            currentLight = .red
+            redView.alpha = 1
+            sender.setTitle("NEXT", for: .normal)
+        }
     }
     
     private func setupView(_ view: UIView, withColour colour: UIColor) {
         view.backgroundColor = colour
         view.layer.cornerRadius = view.frame.size.width / 2
         view.alpha = 0.3
+    }
+    
+    private func switchLight(from currentView: UIView, to nextView: UIView) {
+        currentView.alpha = 0.3
+        nextView.alpha = 1
     }
 }
 
